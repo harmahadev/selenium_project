24fd8e6371f86b1485ec60223a7a4b195e4e6562
harmahadev <akash.khakare91@gmail.com>
2023-06-11 16:16:45 +0530

    first commit

---
+++ src/test/java/com/omayo/testcases/VerifyHomePage.java
@@ -0,0 +1,80 @@
+package com.omayo.testcases;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.codemind.utility.Utility;
+import com.omayo.pages.OmayoHomePage;
+
+public class VerifyHomePage {
+
+	WebDriver driver;
+	OmayoHomePage omayoHomePage;
+	SoftAssert softAssert;
+
+	@BeforeMethod
+	public void setUp() throws IOException {
+		this.driver = Utility.getWebDriver();
+		driver.get(Utility.getProperty("url"));
+		driver.manage().window().maximize();
+		omayoHomePage = new OmayoHomePage(driver);
+		softAssert = new SoftAssert();
+	}
+
+	@Test
+	public void verifyUserAbleToSelectOptionsFromDropdown() throws IOException {
+		// hard aasertion
+		/*
+		 * assertEquals(omayoHomePage.getHomePageTitle(), "omayo (QAFox.com)");
+		 * 
+		 * assertTrue(omayoHomePage.getHomePageTitle().equals("omayo (QAFox.com)"));
+		 * 
+		 * assertNotEquals(omayoHomePage.getHomePageTitle(), "omayo (QAFox.com)");
+		 * 
+		 * System.out.println("After assertion failure");
+		 * 
+		 * assertFalse(omayoHomePage.getHomePageTitle().equals("omayo (QAFox"));
+		 */
+
+		softAssert.assertEquals(omayoHomePage.getHomePageTitle(), "omayo (QAFox.com)");
+
+		softAssert.assertTrue(omayoHomePage.getHomePageTitle().equals("omayo (QAFox.com)"));
+
+		// softAssert.assertNotEquals(omayoHomePage.getHomePageTitle(), "omayo
+		// (QAFox.com)");
+
+		System.out.println("After assertion failure");
+
+		softAssert.assertFalse(omayoHomePage.getHomePageTitle().equals("omayo (QAFox"));
+
+		// verify page url
+		softAssert.assertEquals(omayoHomePage.getHomePageUrl(), "http://omayo.blogspot.com/");
+
+		assertEquals(omayoHomePage.getOnePageLink().getText(), "Page On");
+
+		softAssert.assertFalse(omayoHomePage.getMaleRadioButton().isSelected());
+		
+		Utility.takesScreenshot(driver, "omayoHomePage");
+
+		softAssert.assertAll();
+	}
+
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException {
+		if (result.getStatus()==ITestResult.FAILURE)
+		{
+			Utility.takesScreenshot(driver, result.getName());
+
+		}
+		
+		driver.quit();
+	}
+}
